5358570ed3895ef9551f3ae6c435e8333e5ca132
Perry Mitchell <perry@perrymitchell.net>
2018-02-27 11:30:47 +0200

    Start rework

--- source/menu.js
+++ source/menu.js
@@ -1,34 +1,56 @@
-const Menu = require("terminal-menu");
+const Box = require("cli-box");
+const createList = require("select-shell");
 const { getEntries } = require("./entries.js");
+
+function box(text) {
+    const b = new Box(text);
+    return b.toString();
+}
 
 function displayMainMenu() {
     return getEntries()
         .then(entries => {
-            const menu = Menu({ width: 30, x: 4, y: 2 });
-            menu.reset();
-            menu.write("NPM User Account Switcher\n");
-            menu.write("-------------------------\n");
-            entries.forEach(entry => {
-                menu.add(`Login: ${entry.getProperty("title")}`);
+            console.log(box("NPM User Account Switcher"));
+            const list = createList({
+                checked: "",
+                unchecked: ""
             });
-            menu.add("Add account");
-            menu.add("Delete account");
-            menu.add("Logout");
-            menu.add("Purge");
-            menu.add("Exit");
+            var stream = process.stdin;
+            entries.forEach(entry => {
+                list.option(`Login: ${entry.getProperty("title")}`);
+            });
+            list.option("Exit");
+            list.list();
             return new Promise((resolve, reject) => {
-                menu.on("select", function (label) {
-                    menu.close();
-                    console.log('SELECTED: ' + label);
+                list.on("select", options => {
+                    console.log(options);
+                    resolve();
                 });
-                process.stdin.pipe(menu.createStream()).pipe(process.stdout);
-                process.stdin.setRawMode(true);
-                menu.on("close", function () {
-                    process.stdin.setRawMode(false);
-                    process.stdin.end();
+                list.on("cancel", function() {
                     resolve();
                 });
             });
+            // entries.forEach(entry => {
+            //     menu.add(`Login: ${entry.getProperty("title")}`);
+            // });
+            // menu.add("Add account");
+            // menu.add("Delete account");
+            // menu.add("Logout");
+            // menu.add("Purge");
+            // menu.add("Exit");
+            // return new Promise((resolve, reject) => {
+            //     menu.on("select", function (label) {
+            //         menu.close();
+            //         console.log('SELECTED: ' + label);
+            //     });
+            //     process.stdin.pipe(menu.createStream()).pipe(process.stdout);
+            //     process.stdin.setRawMode(true);
+            //     menu.on("close", function () {
+            //         process.stdin.setRawMode(false);
+            //         process.stdin.end();
+            //         resolve();
+            //     });
+            // });
         });
 }
 
--- source/prompt.js
+++ source/prompt.js
@@ -1,20 +1,42 @@
-const prompt = require("password-prompt");
+// const prompt = require("password-prompt");
+const prompt = require("prompt");
+const pify = require("pify");
+
+const getInput = pify(prompt.get);
 
 function getPassword(confirm = false) {
-    return prompt("Password: ", { method: "hide" })
-        .then(password => {
-            if (confirm) {
-                return prompt("Password (confirm): ", { method: "hide" })
-                    .then(again => {
-                        if (again !== password) {
-                            console.log("Password's didn't match! Please try again...");
-                            return getPassword(true);
-                        }
-                        return password;
-                    });
-            }
-            return password;
+    const schema = [{
+        name: "Password",
+        hidden: true,
+        required: true,
+        replace: "*"
+    }];
+    if (confirm) {
+        schema.push({
+            name: "Password (confirm)",
+            hidden: true,
+            required: true,
+            replace: "*"
         });
+    }
+    return getInput(schema)
+        .then(results => {
+            console.log(results);
+        });
+    // return prompt("Password: ", { method: "mask" })
+    //     .then(password => {
+    //         if (confirm) {
+    //             return prompt("Password (confirm): ", { method: "mask" })
+    //                 .then(again => {
+    //                     if (again !== password) {
+    //                         console.log("Password's didn't match! Please try again...");
+    //                         return getPassword(true);
+    //                     }
+    //                     return password;
+    //                 });
+    //         }
+    //         return password;
+    //     });
 }
 
 module.exports = {
